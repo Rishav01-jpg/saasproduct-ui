00ad1b0d87179015d3c63703893fbbf18d5d9699
Rishav01-jpg <kalart496@gmail.com>
2026-03-20 22:46:13 +0530

    logo updated

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -456,8 +456,8 @@ export default function Landing() {
           }
 
           .logo-img {
-            width: 40px;
-            height: 40px;
+            width: 80px;
+            height: 80px;
           }
 
           .nav-links button {
